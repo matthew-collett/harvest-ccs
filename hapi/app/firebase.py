864b09eccae0e4a66635a92b4c3990953af4f10e
matthew-collett <matthew.collett12@gmail.com>
2025-03-27 23:37:13 -0300

    fix: Fix firebase issue

--- hapi/app/firebase.py
+++ hapi/app/firebase.py
@@ -1,17 +1,19 @@
 import os
 import json
+import base64
 import firebase_admin
 from firebase_admin import credentials
 
-
 if os.environ.get('AWS_LAMBDA_FUNCTION_NAME') is not None:
-    cred_dict = json.loads(os.environ.get('FIREBASE_SERVICE_ACCOUNT'))
+    firebase_creds_b64 = os.environ.get('FIREBASE_SERVICE_ACCOUNT_B64')
+    creds_json = base64.b64decode(firebase_creds_b64).decode('utf-8')
+    cred_dict = json.loads(creds_json)
     cred = credentials.Certificate(cred_dict)
+
 else:
     current_dir = os.path.dirname(os.path.abspath(__file__))
     service_account_path = os.path.join(
         current_dir, 'firebase-service-account.json')
     cred = credentials.Certificate(service_account_path)
 
-# Initialize Firebase with the appropriate credentials
 firebase_app = firebase_admin.initialize_app(cred)
